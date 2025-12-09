ec9e8b6ae7b327e326cf0b9c7f9abc576c85271f
Mohammed Babelly <mohammed@nevercode.io>
2025-12-09 12:21:29 +0400

    update color

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.blue),
+        colorScheme: .fromSeed(seedColor: Colors.orangeAccent),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
